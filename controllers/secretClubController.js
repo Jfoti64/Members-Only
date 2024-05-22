4659508ffd86867c7de5d2c838a83d88cb221096
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-05-21 23:44:39 -0500

    Allow user to join secret_club if password is guessed

--- controllers/secretClubController.js
+++ controllers/secretClubController.js
@@ -1,18 +1,38 @@
 const asyncHandler = require('express-async-handler');
-const moment = require('moment');
+const { body, validationResult } = require("express-validator");
 
 exports.secret_club_get = asyncHandler(async (req, res, next) => {
   try {
-    res.render('secret_club', { title: 'Secret Club',  errors: [] });
+    res.render('secret_club', { title: 'Secret Club', errors: [] });
   } catch (err) {
     next(err);
   }
 });
 
-exports.secret_club_post = asyncHandler(async (req, res, next) => {
-  try {
-    res.render('secret_club', { title: 'Secret Club',  errors: [] });
-  } catch (err) {
-    next(err);
-  }
-});
+exports.secret_club_post = [
+  body('passcode').trim().isLength({ min: 1 }).escape().withMessage('Passcode must be specified.'),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.render('secret_club', {
+        title: 'Secret Club',
+        errors: errors.array()
+      });
+    }
+
+    if (req.body.passcode !== process.env.SECRET_PASSCODE) {
+      return res.render('secret_club', {
+        title: 'Secret Club',
+        errors: [{ msg: 'Incorrect passcode' }]
+      });
+    }
+
+    // Update membership status
+    req.user.membership_status = true;
+    await req.user.save();  // Save the updated user document
+
+    res.redirect('/');
+  })
+];
